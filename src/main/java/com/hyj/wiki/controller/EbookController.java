b6cd1fff2a611f2695ed1fc6a195bc5d388d1334
chosen1hyj <770104718@qq.com>
2021-12-06 19:24:44 +0800

    增加通用返回类

--- src/main/java/com/hyj/wiki/controller/EbookController.java
+++ src/main/java/com/hyj/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.hyj.wiki.controller;
 
 import com.hyj.wiki.domain.Ebook;
+import com.hyj.wiki.resp.CommonResp;
 import com.hyj.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,8 +26,10 @@ public class EbookController {
 
 
     @GetMapping("/list")
-    public List<Ebook> list(){
-
-        return ebookService.list();
+    public CommonResp list(){
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 }
