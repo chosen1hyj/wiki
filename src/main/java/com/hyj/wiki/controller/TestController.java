491c07c90b619409bd60efdb5704a592b0bf6e37
chosen1hyj <770104718@qq.com>
2021-12-06 15:19:31 +0800

    增加hello测试test.http

--- src/main/java/com/hyj/wiki/controller/TestController.java
+++ src/main/java/com/hyj/wiki/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.hyj.wiki.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +17,10 @@ public class TestController {
     @GetMapping("/hello")
     public String hello(){
         return "hello world";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "hello world," + name;
     }
 }
