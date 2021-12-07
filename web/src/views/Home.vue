e6b631721e22ee43223e3d186725a2cb9cb45e03
chosen1hyj <770104718@qq.com>
2021-12-07 20:39:59 +0800

    修改axiosbaseUrl读配置文件

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -97,8 +97,8 @@ export default defineComponent({
 
 
     onMounted(()=>{
-      console.log('onMounted111')
-      axios.get(`${process.env.VUE_APP_SERVER}/ebook/list`).then(
+      console.log('onMounted222')
+      axios.get(`/ebook/list`).then(
           (response)=>{
             const data = response.data;
             ebooks.books = data.content
