86bf70d887e72249fad467804f31c788cb239037
chosen1hyj <770104718@qq.com>
2021-12-07 19:05:47 +0800

    制作the-footer组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -2,19 +2,19 @@
   <a-layout>
     <the-header/>
       <router-view/>
-    <a-layout-footer style="text-align: center">
-      电子书
-    </a-layout-footer>
+    <the-footer/>
   </a-layout>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import TheHeader from '@/components/the-header.vue'
+import TheFooter from '@/components/the-footer.vue'
 export default defineComponent({
   name: 'App',
   components:{
-    TheHeader
+    TheHeader,
+    TheFooter
   }
 });
 </script>
