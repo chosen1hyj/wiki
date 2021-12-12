96a2c6dbcba500804322c187f672f0225e71d765
chosen1hyj <770104718@qq.com>
2021-12-12 20:19:41 +0800

    一些bug

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -1,6 +1,6 @@
 <template>
   <a-layout-header class="header">
-    <div class="logo" />
+    <div class="logo">知识库</div>
     <a-menu
         theme="dark"
         mode="horizontal"
@@ -9,47 +9,35 @@
       <a-menu-item key="/">
         <router-link to="/">首页</router-link>
       </a-menu-item>
-      <a-menu-item key="/admin/ebook" :style="user.id ? {} : {display:'none'}">
+      <a-menu-item key="/admin/user" :style="user.id? {} : {display:'none'}">
+        <router-link to="/admin/user">用户管理</router-link>
+      </a-menu-item>
+      <a-menu-item key="/admin/ebook" :style="user.id? {} : {display:'none'}">
         <router-link to="/admin/ebook">电子书管理</router-link>
       </a-menu-item>
       <a-menu-item key="/admin/category" :style="user.id? {} : {display:'none'}">
         <router-link to="/admin/category">分类管理</router-link>
       </a-menu-item>
-      <a-menu-item key="/admin/user" :style="user.id? {} : {display:'none'}">
-        <router-link to="/admin/user">用户管理</router-link>
-      </a-menu-item>
       <a-menu-item key="/about">
         <router-link to="/about">关于我们</router-link>
-      </a-menu-item>
-
-      <a-menu-item key="login">
-          <a class="login-menu" v-show="!user.id" @click="showLoginModal">
-            登录
-          </a>
-          <a class="login-menu" v-show="user.id">
-            您好:{{user.name}}
-          </a>
       </a-menu-item>
 
       <a-popconfirm
           title="确认退出登录?"
           ok-text="是"
           cancel-text="否"
-          @confirm="logout"
+          @confirm="logout()"
       >
         <a class="login-menu" v-show="user.id">
-          退出登录
+          <span>退出登录</span>
         </a>
       </a-popconfirm>
-<!--      <a class="login-menu" v-show="user.id">-->
-<!--        您好:{{user.name}}-->
-<!--      </a>-->
-<!--      <a-menu-item key="login">-->
-<!--        <a  v-show="!user.id" @click="showLoginModal">-->
-<!--          登录-->
-<!--        </a>-->
-<!--      </a-menu-item>-->
-
+      <a class="login-menu" v-show="user.id">
+        <span>您好：{{user.name}}</span>
+      </a>
+      <a class="login-menu" v-show="!user.id" @click="showLoginModal">
+        <span>登录</span>
+      </a>
     </a-menu>
 
     <a-modal
@@ -67,89 +55,93 @@
         </a-form-item>
       </a-form>
     </a-modal>
-
   </a-layout-header>
 </template>
 
 <script lang="ts">
-import {computed, defineComponent, ref} from 'vue';
-import axios from "axios";
-import {message} from "ant-design-vue";
-import store from '@/store';
+import { defineComponent, ref, computed } from 'vue';
+import axios from 'axios';
+import { message } from 'ant-design-vue';
+import store from "@/store";
 
 declare let hexMd5: any;
 declare let KEY: any;
 
 export default defineComponent({
   name: 'the-header',
-  setup(){
+  setup () {
+    // 登录后保存
+    const user = computed(() => store.state.user);
 
-    //登录后保存
-    const user = computed(()=>store.state.user);
-    //用来登录
+    // 用来登录
     const loginUser = ref({
-      loginName: "test2",
-      password: 'test123'
+      loginName: "test",
+      password: "test"
     });
-
     const loginModalVisible = ref(false);
     const loginModalLoading = ref(false);
     const showLoginModal = () => {
       loginModalVisible.value = true;
     };
+
+    // 登录
     const login = () => {
-      console.log('开始登录');
+      console.log("开始登录");
       loginModalLoading.value = true;
       loginUser.value.password = hexMd5(loginUser.value.password + KEY);
-      axios.post('/user/login', loginUser.value).then(
-          (response) =>{
-            loginModalLoading.value = false;
-            const data = response.data;
-            if(data.success){
-              loginModalVisible.value = false;
-              store.commit("setUser", data.content);
-              // console.log('用户信息：', data.content);
-              message.success("登录成功！");
-            }else{
-              message.error(data.message);
-            }
-          }
-      )
+      axios.post('/user/login', loginUser.value).then((response) => {
+        loginModalLoading.value = false;
+        const data = response.data;
+        if (data.success) {
+          loginModalVisible.value = false;
+          message.success("登录成功！");
+
+          store.commit("setUser", data.content);
+        } else {
+          message.error(data.message);
+        }
+      });
     };
 
+    // 退出登录
     const logout = () => {
-      console.log('退出登录开始');
-      axios.get(`/user/logout/${user.value.token}`).then(
-          (response) =>{
-            const data = response.data;
-            if(data.success){
-              loginModalVisible.value = false;
-              store.commit("setUser", {});
-              message.success("退出登录成功！");
-            }else{
-              message.error(data.message);
-            }
-          }
-      )
+      console.log("退出登录开始");
+      axios.get('/user/logout/' + user.value.token).then((response) => {
+        const data = response.data;
+        if (data.success) {
+          message.success("退出登录成功！");
+          store.commit("setUser", {});
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
 
-    }
-    return{
+    return {
       loginModalVisible,
       loginModalLoading,
       showLoginModal,
       loginUser,
       login,
-      logout,
-      user
+      user,
+      logout
     }
   }
-
 });
 </script>
 
 <style>
+.logo {
+  width: 120px;
+  height: 31px;
+  /*background: rgba(255, 255, 255, 0.2);*/
+  /*margin: 16px 28px 16px 0;*/
+  float: left;
+  color: white;
+  font-size: 18px;
+}
 .login-menu {
-  float: right !important;
+  float: right;
   color: white;
   padding-left: 10px;
 }
