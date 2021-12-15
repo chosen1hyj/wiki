188edf7ddbe74cdd22ac7c733c0f8bc3556b69b0
chosen1hyj <770104718@qq.com>
2021-12-15 21:29:53 +0800

    将业务线程日志流水号传递到异步线程

--- src/main/java/com/hyj/wiki/service/WsService.java
+++ src/main/java/com/hyj/wiki/service/WsService.java
@@ -1,6 +1,7 @@
 package com.hyj.wiki.service;
 
 import com.hyj.wiki.websocket.WebSocketServer;
+import org.slf4j.MDC;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -18,8 +19,8 @@ public class WsService {
     @Resource
     public WebSocketServer webSocketServer;
     @Async
-    public void sendInfo(String message){
-
+    public void sendInfo(String message, String logId){
+        MDC.put("LOG_ID", logId);
         webSocketServer.sendInfo(message);
     }
 
